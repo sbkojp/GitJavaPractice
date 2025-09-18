c354a2c50ebf54f4f70b3e70fe36bcfbc3231299
sbkojp <sbko0617@gmail.com>
2025-09-18 17:03:24 +0900

    add new feature

--- Main.java
+++ Main.java
@@ -3,13 +3,73 @@ package ch4;
 public class Main {
 
 	public static void main(String[] args) {
-		int[]scores = {20,30,40,50,80};
-		int count=0;
-		for(int i=0;i<scores.length;i++) {
-			if(scores[i]>=50) {
-				count++;
+//		String[]fortunes= {"大吉","中吉","吉","末吉","小吉","凶"};
+//		String[]EgFortunes= {"Exselent","luckey","unluckey"};
+//				int[]fortuneNumbers=new int[10];
+//		for(int i=0;i<fortuneNumbers.length;i++) {
+//			fortuneNumbers[i]=(int)(Math.random()*6);
+//		}
+//		for(int num:fortuneNumbers) {
+//			System.out.println(num+":"+fortunes[num]);
+//			System.out.println(num+":"+EgFortunes[num]);
+//			System.out.println();
+//		}
+		int[]arrayA= {1,2,3};
+		int[]arrayB;
+		arrayB=arrayA;
+		arrayB[1]=100;
+		System.out.println(arrayA[1]);
+		
+		
+		int A= 1;
+		int B;
+		B=A;
+		B=100;
+		System.out.println(A);
+		
+		int[]array= {1,2,6};
+		for(int num:array) {
+			int i=1;
+			while(i<=num) {
+				System.out.println(i++);
 			}
-			System.out.println(count);
+		}
+		
+		
+//		int[] seq =new int[10];
+//		for(int i=0;i<seq.length;i++) {
+//			seq[i]=new java.util.Random().nextInt(4);
+//			System.out.print(seq[i]);
+//		}
+//		System.out.println();
+//		char[] base= {'A','T','G','C'};
+//		for(int i=0;i<seq.length;i++) {
+//			System.out.print(base[seq[i]]);
+//		}
+//		System.out.println();
+//		
+//		String[] array= {"大吉","中吉","吉","凶"};
+//		int ran=(int)(Math.random()*4);
+//		System.out.println(array[ran]);
+//		System.out.println(ran);
+//		
+//		for(int num:seq) {
+//			System.out.print(base[num]);//拡張For文
+//		}
+//		
+		
+		
+		
+		
+		
+//		int[]scores = {20,30,40,50,80};
+//		int count=0;
+//		for(int i=0;i<scores.length;i++) {
+//			if(scores[i]>=50) {
+//				count++;
+//			}
+//		}
+//			System.out.println("50点以上の科目の数"+count);
 //			System.out.println(scores[i]);
 //			sum+=scores[i];
 //			if(i==2) {
@@ -23,6 +83,5 @@ public class Main {
 //		System.out.println("合計点"+sum);
 //		System.out.println("平均点"+avg);
 //
-	}
 	}
 }
