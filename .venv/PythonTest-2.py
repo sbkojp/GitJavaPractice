515357ed5a14d4d262a506151efdbbb2a8fe35ea
sbkojp <sbko0617@gmail.com>
2025-10-06 17:01:44 +0900

    P146

--- .venv/PythonTest-2.py
+++ .venv/PythonTest-2.py
@@ -185,5 +185,58 @@ for i in range(10,-20,-1):
     print(i,end=' ')
 print()
 
+for i in range(1,10):
+    if (i % 3) == 0:
+        break
+    print(i)
+else:
+    print('---loop ended---')
 
+for i in range(1,10):
+    if i > 3:
+        break
+    for j in range(1,10):
+        if j > 5:
+            break
+        print(i*j, end=' ')
+    print()
+
+for i in range(1,10):
+    if(i % 3) ==0:
+        continue
+    print(i)
+else:
+    print('---loop ended---')
+
+pin = input('PINを入力')
+while (pin != '1234'):
+    print('違います')
+    pin = input('PINを入力')
+print('当たりです')
+
+while True:
+    pin = input('PINを入力')
+    if (pin == '1234'):
+        print('当たりです！')
+        break
+    else:
+        print('違います')
+
+x = int(input('数字を入力'))
+if x % 2 == 0:
+    print('偶数です')
+else:
+    print('奇数です')
     
+score = 79
+if 80<=score<=100:
+    print('合格')
+else:
+    print('不合格')
+
+for i in range(1,6):
+    if i==5:
+        break
+    print(i,end='a')
+print(i)
+
